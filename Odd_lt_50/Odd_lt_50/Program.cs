5cbbb101780a1bb581357034d5fba8e3e0c2e682
irf-does-work <irfan.shakir@geekywolf.com>
2024-09-25 15:31:36 +0530

    added odd<50 and largest of 3

---
+++ Odd_lt_50/Odd_lt_50/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_lt_50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers < 50 are: ");
+            int count = 1;
+            while (count < 50)
+            {
+                Console.WriteLine(count);
+                count+=2;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ largestFrom3/largestFrom3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace largestFrom3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3,max;
+            Console.WriteLine("Enter the 3 numbers to be compared: ");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());   
+            num3 = int.Parse(Console.ReadLine());
+            max = num1;
+            
+            if (num2 > max)
+            {
+                max = num2;
+            }
+            if (num3 > max)
+            {
+                max = num3;
+            }
+  
+            Console.WriteLine("The largest number is: " +max);
+            Console.ReadLine();
+        }
+    }
+}
