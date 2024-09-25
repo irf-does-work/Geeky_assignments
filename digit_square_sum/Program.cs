a3f023d555cb028e35382b09625c853ebf8c84f6
irf-does-work <irfan.shakir@geekywolf.com>
2024-09-25 15:47:51 +0530

    Added digit square sum

---
+++ digit_square_sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digit_square_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum=0 , square = 0;
+            Console.WriteLine("Enter the number to calculate Sum of Digit Squares: ");
+            num = int.Parse(Console.ReadLine());
+            while (num > 0)
+            {
+                square = num % 10;
+                square *= square;
+                sum += square ;
+                num = num / 10;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
