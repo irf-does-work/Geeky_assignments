074a898a05622800cd6dcf69fc774a3557902847
irf-does-work <irfan.shakir@geekywolf.com>
2024-09-25 15:39:18 +0530

    added digit sum program

---
+++ digitsum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitsum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0;
+            Console.WriteLine("Enter the number to calculate Digit Sum: ");
+            num = int.Parse(Console.ReadLine());
+            while (num > 0)
+            {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
