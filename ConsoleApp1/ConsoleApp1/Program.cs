634a00bd3e8ab8fc435179ccd670b58b0b3af965
irf-does-work <irfan.shakir@geekywolf.com>
2024-09-25 15:26:31 +0530

    Added print first 20 nums task

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class First20
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First 20 integers:");
+            int count = 1;
+            for (; count <= 20; count++) 
+            {
+                Console.WriteLine(count);
+            }
+
+        }
+    }
+}
