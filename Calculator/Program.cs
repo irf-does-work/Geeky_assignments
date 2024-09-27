954287f0182640f61c3b8e91c75ffbb2847b089f
irf-does-work <irfan.shakir@geekywolf.com>
2024-09-27 12:08:17 +0530

    added employee,animals,Calculator assignments

---
+++ Calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, result=9999;
+            int operation;
+            Console.WriteLine("Enter any two numbers");
+            num1 = double.Parse(Console.ReadLine());
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Select an Operation:\n 1.Addition \n 2.Subtraction \n 3.Multiplication \n 4.Division");
+            operation = int.Parse(Console.ReadLine());
+            Calc c1= new Calc();
+            switch (operation)
+            {
+                case 1: result = c1.Add(num1, num2); break;
+                case 2: result = c1.Sub(num1, num2); break;
+                case 3: result = c1.Mul(num1, num2); break;
+                case 4: result = c1.Div(num1, num2); break;
+                default: Console.WriteLine("An ERROR occured"); break;
+            }
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+
+    }
+
+
+    public class Calc
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ animals/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace animals
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+            Bird b1 = new Bird();
+
+            d1.Sound();
+            c1.Sound();
+            b1.Sound();
+            Console.ReadLine();
+        }
+    }
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal makes a sound.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Dog says Woof");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Cat says Meow");
+        }
+    }
+
+    public class Bird : Animal
+    {     
+        public override void Sound()
+        {
+            Console.WriteLine("Bird says Chirp");
+        }
+    }
+
+    
+}
+
+
