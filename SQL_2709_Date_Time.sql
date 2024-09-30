31331212a76011f95e606e1c4e73b17386000cd0
irf-does-work <irfan.shakir@geekywolf.com>
2024-09-30 18:01:54 +0530

    added 18 queries in sql date time

---
+++ SQL_2709_Date_Time.sql
@@ -0,0 +1,59 @@
+----------------DATE AND TIME FUNCTIONS---------------
+
+
+--37. Write a query to display the current date and time.
+SELECT GETDATE();
+
+--38. From an 'Orders' table, find all orders placed in the last 24 hours.
+SELECT * FROM Orders WHERE OrdDate >= DATEADD(HOUR,-24,GETDATE());
+
+--39. Display the current UTC date and time.
+SELECT GETUTCDATE();
+
+--40. Write a query to show the time difference between local time and UTC time.
+SELECT DATEDIFF(MINUTE,GETDATE(), GETUTCDATE());
+
+--41. Convert the current date and time to 'Pacific Standard Time'.
+SELECT GETDATE() AT TIME ZONE 'Pacific Standard Time';
+
+--42. From a 'Flights' table with a 'DepartureTime' column in UTC, convert all departure times to 'Eastern Standard Time'.
+UPDATE Flights SET DepartureTime = DepartureTime AT TIME ZONE  'Eastern Standard Time';
+
+--43. Add 3 months to the current date.
+SELECT DATEADD(MONTH,3,GETDATE());
+
+--44. From an 'Employees' table, write a query to calculate each employee's retirement date (65 years from their 'DateOfBirth').
+SELECT EmpName, DATEADD(YEAR,65,DoB) as Retirement FROM Employees;
+
+--45. Calculate the number of days between '2023-01-01' and '2023-12-31'.
+SELECT DATEDIFF(DAY,2023-12-31,2023-01-01);
+
+--46. From an 'Orders' table, find the average number of days between order date and ship date.
+SELECT AVG(DATEDIFF(DAY,ShipDate,OrderDate));
+
+--47. Extract the month number from the date '2023-09-15'.
+SELECT MONTH('2023-09-15');
+
+--48. From a 'Sales' table, write a query to group total sales by the quarter of the sale date.
+SELECT DATEPART(QUARTER,SaleDate) AS Quarter, SUM(Quantity * UnitPrice) FROM Sales GROUP BY DATEPART(QUARTER,SaleDate);
+
+--49. Extract the year from the current date.
+SELECT YEAR(GETDATE());
+
+--50. From an 'Employees' table, find all employees hired in the year 2022.
+SELECT FirstName,HireDate FROM Employees WHERE YEAR(HireDate) = 2022;
+
+--51. Check if '2023-02-30' is a valid date.
+SELECT ISDATE('2023-02-30');
+
+--52. Write a query to find all rows in a 'UserInputs' table where the 'EnteredDate' column contains invalid dates.
+SELECT * FROM UserInputs WHERE ISDATE(EnteredDate) = 0;
+
+--53. Find the last day of the current month.
+SELECT EOMONTH(GETDATE());
+
+--54. From a 'Subscriptions' table, write a query to extend all subscription end dates to the end of their respective months.
+UPDATE Subscriptions SET EndDate = EOMONTH(EndDate);
+
+--55. Display the current date and time.
+SELECT GETDATE();
