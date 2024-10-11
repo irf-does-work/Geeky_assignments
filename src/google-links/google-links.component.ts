309badb6d2d9385a920228e10e1519467f14c9e8
irf-does-work <irfan.shakir@geekywolf.com>
2024-10-11 18:19:50 +0530

    Added angular assignment(routing)

---
+++ src/google-links/google-links.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-links',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-links.component.html',
+  styleUrl: './google-links.component.scss'
+})
+export class GoogleLinksComponent {
+
+}
