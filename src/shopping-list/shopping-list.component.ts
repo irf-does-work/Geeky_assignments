6cdeba2ae61e2819ff7f576924820622913594d4
irf-does-work <irfan.shakir@geekywolf.com>
2024-10-17 11:39:05 +0530

    Update shopping-list.component.ts

--- src/shopping-list/shopping-list.component.ts
+++ src/shopping-list/shopping-list.component.ts
@@ -1,35 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
-   NewItem(): void {
-    //alert("button clicked");
-    var unorderedList = document.getElementById("list1");
-    var inputField = document.getElementById("input1") as HTMLInputElement;
-    var item = inputField!.value; //sets item as the value of input field
-    //console.log(item);
+  item : string ='';
+  itemList : string[] =[];
 
-    var listItem = document.createElement("li");
-    listItem.appendChild(document.createTextNode(item));
-    unorderedList!.appendChild(listItem);
+  newItem(): void {
+    this.itemList.push(this.item);
+  }
 
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = "DELETE";
-    deleteButton.id = "DeleteButton";
-    deleteButton.addEventListener('click', function() {
-        unorderedList!.removeChild(listItem); // Remove the list item
-    });
-    listItem.appendChild(deleteButton);
-
-
-    
-}
-
-
+  deleteItem(index: number){
+    this.itemList.splice(index,1);
+  }
 }
